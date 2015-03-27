4c0b7fd52e3c854d51abda5a9782f716984a87bf
Markus Michael Geipel <markus@i-storm.de>
2015-03-27 11:27:35 +0100

    Revert "adapted to LoggerFactory"
    This reverts commit a63ee318dadcfeb4fb60b53ba6bdd936975f1c7b.

--- src/main/java/com/siemens/oss/omniproperties/ShellExecutor.java
+++ src/main/java/com/siemens/oss/omniproperties/ShellExecutor.java
@@ -4,25 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.siemens.oss.omniproperties.util.ReflectionUtil;
-
 
 /**
  * This class calls a "command" in the currently active shell. The 'shellCommand' must
  * be given to the constructor of this class. Therefore this class can be used
  * for any kind of preprocessing steps outside of Java.    
  * 
+ * The output of the shell is written to STDOUT. 
  * @author Michel Tokic
  */
 public final class ShellExecutor implements Runnable {
 	
 	private String shellCommand = "";
-	
-	private static final Logger LOG = LoggerFactory
-			.getLogger(ShellExecutor.class);
 	
 	public ShellExecutor (String shellCommand) {
 		this.shellCommand = shellCommand;
@@ -31,7 +24,7 @@ public final class ShellExecutor implements Runnable {
 	public void run() {
 		try {
 			// execute shell command
-			LOG.info(" => executing command '" + this.shellCommand + "'");
+			System.out.println (" => executing command '" + this.shellCommand + "'");
 			Process p = Runtime.getRuntime().exec(this.shellCommand);			
             
 			// write shell output to console output
@@ -39,7 +32,7 @@ public final class ShellExecutor implements Runnable {
             String line=reader.readLine();
 
             while (line != null) {    
-            	LOG.info(line);
+                System.out.println(line);
                 line = reader.readLine();
             }
 			
